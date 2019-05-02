e54c11e5054257c7803737202b39b1a375ac38c0
Walee <waleemamun@gmail.com>
2019-05-02 16:08:19 -0400

    updating tree java

--- Tree.java
+++ Tree.java
@@ -1,11 +1,23 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Tree {
-    TreeNode root;
+    public TreeNode root;
     public Tree (int rootVal){
         root = new TreeNode(rootVal);
     }
     public Tree () {
         root = new TreeNode(Integer.MIN_VALUE); // creating a tree but the root value is not init
     }
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public void setRoot(TreeNode root) {
+        this.root = root;
+    }
+
     public void createBinaryTree (int arr[]) {
         if (arr.length == 0) {
             System.out.println("Array size zero");
@@ -16,6 +28,19 @@ public class Tree {
         root.rightChild = createBinaryTreeRecurse(mid+1, arr.length-1,arr);
 
     }
+
+    public TreeNode createBSTree (int low , int high, int arr []){
+        if (low > high) {
+            return null;
+        }
+        int mid = (low + high)/2;
+        TreeNode node = new TreeNode(arr[mid]);
+        System.out.println("Creating node "+ node.data + "[" + low +"," + high + "] " + mid);
+        node.leftChild = createBSTree(low, mid-1, arr);
+        node.rightChild = createBSTree(mid+1, high, arr);
+        return node;
+    }
+
     private TreeNode createBinaryTreeRecurse (int low, int high, int arr []) {
         if (low > high) {
             return null;
@@ -27,6 +52,7 @@ public class Tree {
         node.rightChild = createBinaryTreeRecurse(mid+1, high, arr);
         return node;
     }
+
     public void inOrderTraversal (TreeNode node) {
         if ( node != null) {
             inOrderTraversal(node.leftChild);
@@ -34,11 +60,37 @@ public class Tree {
             inOrderTraversal(node.rightChild);
         }
     }
+
     public void preOrderTraversal (TreeNode node) {
         if (node != null) {
             System.out.print(node.data + " ");
             preOrderTraversal(node.leftChild);
             preOrderTraversal(node.rightChild);
         }
+    }
+    public void levelOrderTraversalAdd2List () {
+        ArrayList<LinkedList<TreeNode>> listArray = new ArrayList<>();
+        int level = 0;
+        LinkedList<TreeNode> list  = new LinkedList();
+        list.add(root);
+        listArray.add(level,list);
+
+        while (true) {
+            list = new LinkedList<>();
+            for (int i = 0; i < listArray.get(level).size(); i++) {
+                TreeNode node = (TreeNode) listArray.get(level).get(i);
+                if (node.leftChild != null)
+                    list.add(node.leftChild);
+                if (node.rightChild != null)
+                    list.add(node.rightChild);
+            }
+            if (list.size() == 0) {
+                break;
+            }
+            level++;
+            listArray.add(level,list);
+
+        }
+
     }
 }
